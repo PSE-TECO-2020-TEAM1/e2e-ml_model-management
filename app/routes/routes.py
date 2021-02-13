a5ed0b9abb6fed0ac32f7bba8d4e2a9fc5d53c4a
Atalay Donat <atalaydonatt@gmail.com>
2021-02-13 20:29:36 +0300

    Added routes
    Co-authored-by: Mustafa Enes Batur <mustafa.enes99@gmail.com>

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -17,34 +17,10 @@ from app.models.responses import (
 
 router = APIRouter(prefix="/api")
 
-@router.get("/Parameters", response_model = GetParameterRes)
-async def getPara():
-    para: GetParameterRes
-    return para
+@router.get("/parameters", response_model = GetParameterRes)
+async def parameters():
+    return
 
-@router.get("/workspaces/{workspaceId}/models", response_model = GetModelsRes)
-async def getModel():
-    models: GetModelsRes
-    return models
-
-@router.post("/workspaces/{workspaceId}/train")
-async def getModel(trainReq: TrainReq):
-    return True
-
-@router.get("/workspaces/{workspaceId}/trainingProgress", response_model = TrainingProgress)
-async def getModel():
-    trainingProgress: TrainingProgress
-    return trainingProgress
-
-@router.get("/workspaces/{workspaceId}/models/{modelId}", response_model = ModelRes)
-async def getModel():
-    model: ModelRes
-    return model
-
-@router.get("/workspaces/{workspaceId}/models/{modelId}/predictionId", response_model = PredictionIDRes)
-async def getModel():
-    predictionID: PredictionIDRes
-    return predictionID
 
 @router.get("/predictionConfig", response_model = PredictionConfigRes)
 async def getModel(predictionConfigreq: GetPredictionConfig):
--- app/routes/secure_routes.py
+++ app/routes/secure_routes.py
@@ -1,9 +1,10 @@
 from app.models.mongo_model import OID
-from fastapi import APIRouter, Header, HTTPException
+from fastapi import APIRouter, Header, HTTPException, status
 from fastapi.param_functions import Depends
 import jwt
 
 import app.models.requests as requests
+import app.models.responses as responses
 
 async def extract_userId(auth_header: str = Header(None)) -> str:
     try:
@@ -13,7 +14,32 @@ async def extract_userId(auth_header: str = Header(None)) -> str:
 
 router = APIRouter(prefix="/api")
 
-@router.post("/workspaces/createModelWorkspace")
+@router.post("/workspaces/createModelWorkspace", status_code=status.HTTP_201_CREATED)
 async def create_model_workspace(req: requests.PostCreateWorkspaceReq, userId: OID = Depends(extract_userId)):
     #TODO sample route
+    pass
+
+@router.get("/workspaces/{workspaceId}/models", response_model = responses.GetModelsRes, status_code=status.HTTP_200_OK)
+async def get_models(workspaceId: OID, userId: OID = Depends(extract_userId)):
+    #TODO
+    pass
+
+@router.post("/workspaces/{workspaceId}/train", status_code=status.HTTP_200_OK)
+async def post_train(req: requests.postTrainReq, userId: OID = Depends(extract_userId)):
+    #TODO
+    pass
+
+@router.get("/workspaces/{workspaceId}/trainingProgress", response_model = responses.GetTrainingProgressRes, status_code=status.HTTP_200_OK)
+async def get_training_progress(workspaceId: OID, userId: OID = Depends(extract_userId)):
+    #TODO
+    pass
+
+@router.get("/workspaces/{workspaceId}/models/{modelId}", response_model = responses.GetModelRes, status_code=status.HTTP_200_OK)
+async def get_model(workspaceId: OID, modelId: OID, userId: OID = Depends(extract_userId)):
+    #TODO
+    pass
+
+@router.get("/workspaces/{workspaceId}/models/{modelId}/predictionId", response_model = responses.GetPredictionIdRes, status_code=status.HTTP_200_OK)
+async def get_prediction_id(workspaceId: OID, modelId: OID, userId: OID = Depends(extract_userId)):
+    #TODO
     pass
