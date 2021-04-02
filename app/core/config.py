a8617472dd944ac921ec1c39f578790d7c0cc7fd
Mustafa Enes Batur <mustafa.enes99@gmail.com>
2021-04-02 22:21:59 +0200

    Implemented config

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,11 @@
 from starlette.config import Config
 
 config = Config(".env")
+
+DATABASE_URI = config("DATABASE_URI")
+DATABASE_PORT = config("DATABASE_PORT", cast=int)
+DATABASE_NAME = config("DATABASE_NAME")
+LISTEN_IP = config("HOST")
+LISTEN_PORT = config("LISTEN_PORT", cast=int)
+AUTH_SECRET = config("AUTH_SECRET")
+WORKSPACE_MANAGEMENT_IP_PORT = config("WORKSPACE_MANAGEMENT_IP_PORT")
