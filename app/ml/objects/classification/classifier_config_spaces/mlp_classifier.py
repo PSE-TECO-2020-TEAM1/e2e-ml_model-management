78d7e698b58ae8dca5d8dd7ff4b5b45e2e52fbd2
Mustafa Enes Batur <mustafa.enes99@gmail.com>
2021-04-12 13:41:44 +0200

    Bugfix

--- app/ml/objects/classification/classifier_config_spaces/mlp_classifier.py
+++ app/ml/objects/classification/classifier_config_spaces/mlp_classifier.py
@@ -5,7 +5,7 @@ from ConfigSpace.hyperparameters import (CategoricalHyperparameter,
                                          UniformFloatHyperparameter,
                                          UniformIntegerHyperparameter)
 cs = ConfigurationSpace()
-hidden_layer_depth = UniformIntegerHyperparameter(name="hidden_layer_depth",
+hidden_layer_sizes = UniformIntegerHyperparameter(name="hidden_layer_sizes",
                                                   lower=1, upper=3, default_value=1)
 num_nodes_per_layer = UniformIntegerHyperparameter(name="num_nodes_per_layer",
                                                    lower=16, upper=264, default_value=32,
@@ -44,7 +44,7 @@ epsilon = Constant(name="epsilon", value=1e-8)
 # max_fun --> only used when solver=lbfgs
 # activation=["identity", "logistic"] --> not useful for classification
 
-cs.add_hyperparameters([hidden_layer_depth, num_nodes_per_layer,
+cs.add_hyperparameters([hidden_layer_sizes, num_nodes_per_layer,
                         activation, alpha,
                         learning_rate_init, early_stopping,
                         n_iter_no_change, validation_fraction, tol,
